5c84047925050ffa2dab9ed5b9af8b103402dfc0
Mark Chester <mark@chesterfamily.org>
2020-02-06 19:01:12 -0800

    Lots of improvements; added cut_from_start and Cut_from_end parameters

--- make-pan.sh
+++ make-pan.sh
@@ -19,27 +19,28 @@ increment=${3:-25}
 # Set frame rate to 30 if not specified
 framerate=${4:-30}
 # Set the first frame to bigger than zero
-first_frame=${5:-0}
-# Set the last frame position, i.e.: "max_frame_position - last_frame"
-last_frame=${6:-0}
+cut_from_start=${5:-0}
+# Set the last frame by cutting off this many from the end
+cut_from_end=${6:-0}
+
+# Aspect ratio
+aspect_width=16
+aspect_height=9
+
+# Can be any file type compatible with H265 video.
+output_file_extension="mkv"
 
 # We need to know the file name bits in order to create the output file name.
 pano_file_extension=$(basename ${pano_file_name} | cut -d '.' -f 2)
 output_file="$(basename -s .${pano_file_extension} ${pano_file_name})"
-# Can be any file type compatible with H264 video.
-output_file_extension="mkv"
 
 # Need to know the dimensions of the pano
-pano_geometry=$(magick identify ${pano_file_name} | awk '{ print $3 }')
+pano_geometry=$(magick identify ${pano_file_name} 2>> make-pan-error.log | awk '{ print $3 }')
 pano_width=$(echo ${pano_geometry} | cut -d x -f 1)
 pano_height=$(echo ${pano_geometry} | cut -d x -f 2)
 
-# Frame dimensions
-aspect_width=16
-aspect_height=9
-
+# We want the pano wide enough for at least one second of video
 verify_wide(){
-    # We want the pano wide enough for at least one second of video
     min_width=$(( (($pano_height / $aspect_height) * $aspect_width) + ( $framerate * $increment) ))
     if [[ ${pano_width} -lt ${min_width} ]]
     then
@@ -49,8 +50,8 @@ verify_wide(){
     fi
 }
 
+# We want the pano tall enough for at least one second of video
 verify_tall(){
-    # We want the pano tall enough for at least one second of video
     min_height=$(( (($pano_width / $aspect_width) * $aspect_height) + ( $framerate * $increment) ))
     if [[ ${pano_height} -lt ${min_height} ]]
     then
@@ -61,32 +62,34 @@ verify_tall(){
 }
 
 # When panning horizontally, assume frame height is the same as the pano
+# Derive the width from the height, and count how many fit in the pano
 set_h_framing(){
     frame_height=$pano_height
     frame_width=$(( ($pano_height / $aspect_height) * $aspect_width ))
-    frame_count=$(( (($pano_width - $frame_width) / $increment) - $first_frame - $last_frame ))
-    make_frames
+    frame_all=$(( ($pano_width - $frame_width) / $increment ))
 }
 
 # When panning vertically, assume frame width is the same as the pano
+# Derive the height from the width, and count how many fit in the pano
 set_v_framing(){
     frame_width=$pano_width
     frame_height=$(( ($pano_width / $aspect_width) * $aspect_height ))
-    frame_count=$(( (($pano_height - $frame_height) / $increment) - $first_frame - $last_frame ))
-    make_frames
+    frame_all=$(( ($pano_height - $frame_height) / $increment ))
 }
 
-# Chops up the pano into still frames
+# Chop up the pano into still frames
 make_frames(){
-    frame=$(( $first_frame ))
+    frame=$(( $cut_from_start ))
     frame_position=$(( $frame * $increment ))
+    frame_count=$(( $frame_all - $cut_from_start - $cut_from_end ))
+    last_frame=$(( $cut_from_start + $frame_count ))
 
-    echo "Panorama size: $pano_width x $pano_height"
+    echo "Panorama size: $pano_width x $pano_height; $frame_all total possible frames."
     echo "Raw frame size: $frame_width x $frame_height (will be scaled to 1920 x 1080)"
     echo "Approximate video length: $(( $frame_count / $framerate )).$(( ($frame_count % $framerate) * 100 / $framerate )) second(s); $frame_count frames."
     echo ""
 
-    while [[ $frame -le $frame_count ]]
+    while [[ $frame -le $last_frame ]]
     do
         # Set the offset along the pano
         frame_position=$(($frame * $increment))
@@ -99,15 +102,15 @@ make_frames(){
             frame_data="${frame_width}x${frame_height}+0+${frame_position}"
         fi
         # Run it!
-        magick ${pano_file_name} -crop ${frame_data} -resize 1920x1080 pan${padded_frame}.png
-        echo "Frame number: ${padded_frame}"
-        echo ""
+        magick ${pano_file_name} -crop ${frame_data} -resize 1920x1080 pan${padded_frame}.png 2>> make-pan-error.log
+        echo -en "Frame number: ${padded_frame}\r"
         # And count
         frame=$(($frame + 1))
     done
+    echo ""
 }
 
-# Panning left or up requires simply reversing a right or down frame sequence, respectively.
+# Panning left or up requires simply reversing a right or down frame sequence.
 reverse_sequence(){
     file_list=$(ls -1 pan*.png | sort --reverse)
     counter=0
@@ -130,14 +133,15 @@ make_video(){
     fi
     ffmpeg -framerate ${framerate} -i ${input_file_pattern} \
     -c:v libx265 -crf 0 -s 1920x1080 ${output_file}.${output_file_extension} \
-    -c:v libx265 -crf 30 -s 1280x720 ${output_file}-preview.${output_file_extension}
+    -c:v libx265 -crf 30 -s 1280x720 ${output_file}-preview.${output_file_extension} \
+    2>> make-pan-error.log
 }
 
 clean_mkv(){
     if [[ ${output_file_extension} == mkv ]]
     then
         mv ${output_file}.${output_file_extension} ${output_file}-dirty.${output_file_extension}
-        mkclean --remux ${output_file}-dirty.${output_file_extension} ${output_file}.${output_file_extension}
+        mkclean --remux ${output_file}-dirty.${output_file_extension} ${output_file}.${output_file_extension}  2>> make-pan-error.log
         rm -f ${output_file}-dirty.${output_file_extension}
     fi
 }
